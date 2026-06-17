e8ee79e17320c7894605a700a6d7a87c69d3786a
VoluteTech <chewie.louveau@gmail.com>
2026-06-17 16:53:26 +0200

    feat: add routing && template rendering to create cards

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -21,4 +22,36 @@ func renderTemplate(w http.ResponseWriter, filename string, data interface{}) {
 }
 
 func main() {
+	http.HandleFunc("GET /", handleHome)
+	http.HandleFunc("GET /cards/new", handleNewCardForm)
+	http.HandleFunc("GET /cards/cancel", handleFormCancel)
+	http.HandleFunc("POST /cards", handleCreateCard)
+
+	log.Println("Server starting on http://localhost:42069")
+	log.Fatal(http.ListenAndServe(":42069", nil))
 }
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.html", nil)
+}
+
+func handleNewCardForm(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "form.html", nil)
+}
+
+func handleFormCancel(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "button.html", nil)
+}
+
+func handleCreateCard(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	if title == "" {
+		http.Error(w, "No title provided", http.StatusBadRequest)
+		return
+	}
+
+	card := Card{ Title: title }
+	renderTemplate(w, "response.html", card)
+}
+
+
