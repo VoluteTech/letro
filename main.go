e9067f9cfd89cca862cee01f8db4481330d273e4
VoluteTech <chewie.louveau@gmail.com>
2026-06-17 16:34:18 +0200

    feat: add renderTemplate utility function

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+type Card struct {
+	Title string
+}
+
+func renderTemplate(w http.ResponseWriter, filename string, data interface{}) {
+	path := filepath.Join("templates", filename)
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(w, "Template not found: " +err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, data)
+}
 
 func main() {
-	fmt.Println("oui")
 }
