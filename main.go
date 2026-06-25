86ed1ff3c37f50acc0fb95a9fc83293827c64ced
VoluteTech <chewie.louveau@gmail.com>
2026-06-25 15:17:34 +0200

    feat: add other columns + description to the card

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 )
 
 type Card struct {
-	Title string
+	Title       string
+	Description string
+}
+
+type TemplateData struct {
+	Column string
+	Card   Card
 }
 
 func renderTemplate(w http.ResponseWriter, filename string, data interface{}) {
 	path := filepath.Join("templates", filename)
 	tmpl, err := template.ParseFiles(path)
 	if err != nil {
-		http.Error(w, "Template not found: " +err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Template not found: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl.Execute(w, data)
@@ -36,22 +42,34 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleNewCardForm(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "form.html", nil)
+	column := r.URL.Query().Get("column")
+	if column == "" {
+		column = "todo"
+	}
+	renderTemplate(w, "form.html", TemplateData{Column: column})
 }
 
 func handleFormCancel(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "button.html", nil)
+	column := r.URL.Query().Get("column")
+	if column == "" {
+		column = "todo"
+	}
+	renderTemplate(w, "button.html", TemplateData{Column: column})
 }
 
 func handleCreateCard(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
+	desc := r.FormValue("description")
+	column := r.FormValue("column")
+	if column == "" {
+		column = "todo"
+	}
 	if title == "" {
 		http.Error(w, "No title provided", http.StatusBadRequest)
 		return
 	}
 
-	card := Card{ Title: title }
-	renderTemplate(w, "response.html", card)
+	card := Card{Title: title, Description: desc}
+	w.Header().Set("HX-Trigger", "restore-form-"+column)
+	renderTemplate(w, "response.html", TemplateData{Column: column, Card: card})
 }
-
-
